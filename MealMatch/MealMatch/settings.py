24505058948b75b2ffa0eccbf4daf0b81b998384
Andreas <andsamuppsala@gmail.com>
2017-05-09 17:13:11 +0200

    I love fucking static and that's my problem

--- MealMatch/MealMatch/settings.py
+++ MealMatch/MealMatch/settings.py
@@ -17,7 +17,7 @@ import mongoengine
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 DBNAME = 'test'
 _MONGODB_USER = 'fabian'
@@ -250,19 +250,21 @@ USE_L10N = True
 USE_TZ = True
 
 
-## Static files (CSS, JavaScript, Images)
-## https://docs.djangoproject.com/en/1.9/howto/static-files/
-#
-#STATIC_URL = '/static/'
-#
-#
-#
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, "static"),
-#    #'/static/css/',
-## ]
-#]
-#
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 
 SOCIAL_AUTH_FACEBOOK_KEY = '765337320295594'  # App ID
