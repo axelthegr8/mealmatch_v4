ee6a2835f12c57fbeb1cba721994c7f5ff8ac8f9
Andreas <andsamuppsala@gmail.com>
2017-05-09 16:57:15 +0200

    test

--- MealMatch/MealMatch/settings.py
+++ MealMatch/MealMatch/settings.py
@@ -211,12 +211,8 @@ WSGI_APPLICATION = 'MealMatch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME' : 'mealmatch',
-        'USER': 'axel',
-        'PASSWORD': 'Flodhast1337',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
